429d4ce161169e542f88e9c0db245994a1dd41f8
muhammad <memomq70@gmail.com>
2023-04-29 19:02:47 +0300

    fixed and added Readme

--- script.py
+++ script.py
@@ -2,7 +2,7 @@
 import concurrent.futures
 import time
 import os
-
+import subprocess
 def getCampaignId():
     with open("TracRx_EDH_CROSSIX_Consumer_DimCampaign_Daily_FULL_20170707.txt","r") as f:
         campainId = []
@@ -41,14 +41,35 @@ def getRecord(filename,recname,cmpId):
                     if cmpId in line:
                         add.write(line+"\n")
 
+def insertHeader(cmpID):
+    directory = os.getcwd() + "\\"
+    for i in cmpID:
+        i = i.strip('"') + "\\"
+        path = os.path.join(directory,i)
+        for file in os.listdir(path):
+            val = path+file
+            cmd = subprocess.run(f"sed -i '1iHeader:[{i}]' {val}",shell=True,capture_output=True,text=True)
+
+def Compressfiles(cmpID):
+    directory = os.getcwd() + "\\"
+    for i in cmpID:
+        i = i.strip('"') + "\\"
+        path = os.path.join(directory,i)
+        for file in os.listdir(path):
+            val = path+file
+            cmd = subprocess.run(f"gzip {val}",shell=True,capture_output=True,text=True)
+
 if __name__ == '__main__':
     t1 = time.perf_counter()
     createDirectories()
     cmpID = getCampaignId()
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for item in cmpID:
-            a = executor.submit(getRecord,'UserCenterClicks.txt','NewsletterClicksActivity_', item)
+            a = executor.submit(getRecord,'UserCenterNewsletterClicksActivity.txt','NewsletterClicksActivity_', item)
             b = executor.submit(getRecord,'UserMediaAllActivity.txt','UserMediaAllActivity_', item)
             c = executor.submit(getRecord,'UserTrafficCenterActivity.txt','UserTrafficCenterActivity_', item)
+    insertHeader(cmpID)
+    Compressfiles(cmpID)
+
     t2 = time.perf_counter()
     print(f"Finished in {t2-t1} seconds")
